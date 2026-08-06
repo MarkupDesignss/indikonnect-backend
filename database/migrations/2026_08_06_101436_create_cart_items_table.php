4e1d05a49d4cdcae4d6779640702edef6a0ac3a1
shekhar-markup <ramashankar.yadav@markupmail.com>
2026-08-06 15:48:05 +0530

    Working on databse migrations

---
+++ database/migrations/2026_08_06_101436_create_cart_items_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cart_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->integer('quantity');
+            $table->decimal('unit_price', 12, 2);
+            $table->timestamps();
+
+            $table->index('cart_id');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
