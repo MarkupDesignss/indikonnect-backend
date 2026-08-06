c19dc1a9b568cd2523e65c935e499145b764bb27
kishan <kishan.chauhan@markupmail.com>
2026-08-06 13:42:11 +0530

    made changes in auth apis

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('full_name')->nullable();
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
             $table->string('phone')->nullable();
             $table->string('password')->nullable();
             $table->string('country')->nullable();
