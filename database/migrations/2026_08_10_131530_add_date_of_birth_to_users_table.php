cdab3474eebffdaac9192ef8818d8619ee446535
kishan <kishan.chauhan@markupmail.com>
2026-08-10 18:45:51 +0530

    change the entire auth

--- database/migrations/2026_08_10_131530_add_date_of_birth_to_users_table.php
+++ database/migrations/2026_08_10_131530_add_date_of_birth_to_users_table.php
@@ -14,10 +14,9 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->timestamp('date_of_birth')
                 ->nullable()
-                ->change();
+                ->after('country');
         });
     }
-
 
     /**
      * Reverse the migrations.
