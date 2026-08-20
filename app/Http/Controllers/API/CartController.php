74c4ea7cd0443cadc6f1359c8e1af109bfd01a2d
kishan <kishan.chauhan@markupmail.com>
2026-08-20 17:53:28 +0530

    return done

--- app/Http/Controllers/API/CartController.php
+++ app/Http/Controllers/API/CartController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Cart;
 use App\Models\CartItem;
 use App\Models\Product;
+use App\Models\Wishlist;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
