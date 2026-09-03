d03b373c3e9bc0fb6de400b536557ee45006c382
kishan <kishan.chauhan@markupmail.com>
2026-09-03 17:28:02 +0530

    order details done

---
+++ app/Models/Brand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+
+    protected $fillable = [
+        'title',
+        'discount_percentage',
+        'logo',
+        'banner'
+    ];
+
+    protected $casts = [
+        'discount_percentage' => 'integer',
+    ];
+
+    // Accessor for full logo URL
+    public function getLogoUrlAttribute()
+    {
+        return $this->logo ? asset('storage/' . $this->logo) : null;
+    }
+
+    // Accessor for full banner URL
+    public function getBannerUrlAttribute()
+    {
+        return $this->banner ? asset('storage/' . $this->banner) : null;
+    }
+}
