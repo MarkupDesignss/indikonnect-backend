0884a078b8defb33a8a43090811af5510844894f
shekhar-markup <ramashankar.yadav@markupmail.com>
2026-09-08 11:57:12 +0530

    Distributor creation by admin

--- routes/api.php
+++ routes/api.php
@@ -622,6 +622,9 @@ Route::prefix('admin')->middleware(['auth:sanctum', 'admin'])->group(function ()
     Route::get('/credit-notes/{id}', [CreditNoteController::class, 'show']);
     Route::get('/credit-notes/{id}/download-data', [CreditNoteController::class, 'downloadAdminData']);
     Route::get('/credit-notes/export', [CreditNoteController::class, 'export']);
+
+    // Admin User Management (for distributors)
+    Route::post('/users', [AdminUserController::class, 'store']);
 });
 
 Route::get('admin/orders/statuses', [OrderController::class, 'orderstatuses']);
