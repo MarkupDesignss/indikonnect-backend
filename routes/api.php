949861a40227fdc8c3b74586ce3f4bcc6fa508cc
shekhar-markup <ramashankar.yadav@markupmail.com>
2026-09-09 19:21:06 +0530

    changes in cancellation

--- routes/api.php
+++ routes/api.php
@@ -48,6 +48,7 @@ use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\FAQController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\CancellationApprovalController;
 use App\Http\Controllers\API\BrandController;
 use App\Http\Controllers\API\SubcategoryController;
 use App\Http\Controllers\API\TestimonialController;
