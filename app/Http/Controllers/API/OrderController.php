85136f071beb8606ecc214996f0404b5de882181
kishan <kishan.chauhan@markupmail.com>
2026-09-15 13:01:48 +0530

    login done

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -1706,6 +1706,9 @@ class OrderController extends Controller
                         'shipped_at' => $formatDate($line->shipped_at),
                         'cancelled_at' => $formatDate($line->cancelled_at),
                         'dispatched_at' => $formatDate($line->dispatched_at),
+                        'return_applicable_till' => $line->dispatched_at
+                            ? $formatDate(\Carbon\Carbon::parse($line->dispatched_at)->addDays(30))
+                            : null,
                         'delivered_at' => $formatDate($line->delivered_at),
                         'return_requested_at' => $formatDate($line->return_requested_at),
                         'return_approved_at' => $formatDate($line->return_approved_at),
